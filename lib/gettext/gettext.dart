99c1d2802488b4865c3c27bff5f5f09ade604063
Rahmi Tufanoglu <rahmi.tufanoglu@gmail.com>
2024-08-06 09:43:52 +0200

    test

--- lib/gettext/gettext.dart
+++ lib/gettext/gettext.dart
@@ -109,8 +109,8 @@ class Gettext {
   }) {
     final translation = _getTranslation(domain ?? this.domain, msgctxt, msgid);
 
-    if (translation == null) {
     //if (translation == null || translation.msgstr[0].isEmpty) {
+    if (translation == null) {
       _warn('No translation was found for '
           'msgid "$msgid" in msgctxt "$msgctxt" and domain "$domain"');
       return msgid;
